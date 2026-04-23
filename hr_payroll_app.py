9fc58e429cd72a8697517c46769a5d0c5c1f72e2
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-04-23 14:28:17 +0100

    Update hr_payroll_app.py

--- hr_payroll_app.py
+++ hr_payroll_app.py
@@ -63,7 +63,7 @@ def generate_pdf(name, result):
 
     # ✅ Use ONLY logo.png
     if os.path.exists("logo.png"):
-        logo = Image("logo.png", width=120, height=60)
+        logo = Image("logo.png", width=60, height=60)
         elements.append(logo)
         elements.append(Spacer(1, 10))
 
@@ -85,7 +85,7 @@ def generate_pdf(name, result):
         ["Monthly PAYE", f"{result['Monthly Tax']:,.2f}"],
     ]
 
-    table = Table(data, colWidths=[260, 180])
+    table = Table(data, colWidths=[360, 280])
 
     table.setStyle(TableStyle([
         ("BACKGROUND", (0, 0), (-1, 0), colors.darkblue),
