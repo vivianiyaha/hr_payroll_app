8964373ba848a67d8f50c830ab46bb8e6c9a1095
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-04-23 14:15:21 +0100

    Update hr_payroll_app.py

--- hr_payroll_app.py
+++ hr_payroll_app.py
@@ -133,7 +133,7 @@ with tab1:
                 st.success(f"Annual Tax: ₦{result['Annual Tax']:,.2f}")
                 st.success(f"Monthly PAYE: ₦{result['Monthly Tax']:,.2f}")
 
-            pdf = generate_pdf(name, result, logo_file)
+            pdf = generate_pdf(name, result, logo.png)
 
             st.download_button(
                 label="⬇️ Download Payslip (PDF)",
