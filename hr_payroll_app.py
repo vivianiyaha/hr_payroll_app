08999fd0179036d5ecf259b72a870815cfd36ec6
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-04-23 14:33:58 +0100

    Update hr_payroll_app.py

--- hr_payroll_app.py
+++ hr_payroll_app.py
@@ -85,7 +85,7 @@ def generate_pdf(name, result):
         ["Monthly PAYE", f"{result['Monthly Tax']:,.2f}"],
     ]
 
-    table = Table(data, colWidths=[260, 380])
+    table = Table(data, colWidths=[260, 180])
 
     table.setStyle(TableStyle([
         ("BACKGROUND", (0, 0), (-1, 0), colors.darkblue),
